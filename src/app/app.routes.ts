e7a4341616e236962a2f3e566fe14f36f304173f
12130402 <12130402@me.mcu.edu.tw>
2024-01-15 01:30:01 +0800

    feat(router): 導覽列路由切換實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'sign-in', component: SignInPageComponent },
+  { path: 'sign-up', component: SignUpPageComponent },
+];
---
+++ src/app/sing-in-page/sing-in-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sing-in-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sing-in-page.component.html',
+  styleUrl: './sing-in-page.component.css'
+})
+export class SingInPageComponent {
+
+}
